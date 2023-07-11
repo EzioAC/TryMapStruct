97fa488b2e77b0836a0f4629c439b2f072b8ed85
EzioXiang <1094708529@qq.com>
2023-07-11 20:29:06 +0800

    init

---
+++ src/test/java/com/example/trymapstruct/TryMapStructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trymapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryMapStructApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
